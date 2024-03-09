a36eb2a0ce909322b04b971af8c3e4011f2cf05a
Nguyen TT <trongnguyentran1606@gmail.com>
2024-03-09 21:27:49 +0700

    define some functions to display

---
+++ Project/displayfunction.h
@@ -0,0 +1,10 @@
+#ifndef _DISPLAYFUNCTION_H
+#define _DISPLAYFUNCTION_H
+#include<SFML/Graphics.hpp>
+
+void chooseRole(sf::RenderWindow& window);
+void loginWindow(sf::RenderWindow& window, bool role);
+
+
+
+#endif // !_DISPLAYFUNCTION_H
