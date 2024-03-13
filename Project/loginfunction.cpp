d2b708b06ca81a653a0426fc7620aa6fb03c7e13
Nguyen TT <trongnguyentran1606@gmail.com>
2024-03-13 14:07:08 +0700

    change UI

--- Project/loginfunction.cpp
+++ Project/loginfunction.cpp
@@ -7,7 +7,7 @@ void chooseRole(sf::RenderWindow& window)
 {
     
     sf::Texture roletexture;
-    roletexture.loadFromFile("App Design/[Design 1] Login 1.jpg");
+    roletexture.loadFromFile("App Design/[Design 3] Login 1.jpg");
     roletexture.setSmooth(true);
     sf::Sprite s_roletexture;
     s_roletexture.setTexture(roletexture);
@@ -29,13 +29,13 @@ void chooseRole(sf::RenderWindow& window)
                 bool role;//0 is student, 1 is teacher
                 int x_coor = event.mouseButton.x;
                 int y_coor = event.mouseButton.y;
-                if (y_coor > 865 && y_coor < 935)
+                if (y_coor > 630 && y_coor < 690)
                 {
-                    if (x_coor > 517 && x_coor < 757) {
+                    if (x_coor > 47 && x_coor < 162) {
                         role = 0;
                         loginWindow(window, role);//if enter student button move to login window with student role
                     }
-                    else if (x_coor > 970 && y_coor < 1210) {
+                    else if (x_coor > 232 && y_coor < 347) {
                         role = 1;
                         loginWindow(window, role);
                     }
@@ -50,16 +50,18 @@ void loginWindow(sf::RenderWindow& window, bool role)
 {
     account acc;
     sf::Texture loginscreen;
-    loginscreen.loadFromFile("App Design/[Design 1] Login 2.jpg");
+    loginscreen.loadFromFile("App Design/[Design 3] Login 2.jpg");
     loginscreen.setSmooth(true);
     sf::Sprite s_loginscreen;
     s_loginscreen.setTexture(loginscreen);
     sf::Font rokkitfont;
     rokkitfont.loadFromFile("Font/Rokkitt-VariableFont.ttf");
-    TextBox username(30, sf::Color::White, false);
+    TextBox username(30, sf::Color::Black, false);
     username.setfont(rokkitfont);
-    TextBox password(30, sf::Color::White, false);
+    username.setlimit(true, 20);
+    TextBox password(30, sf::Color::Black, false);
     password.setfont(rokkitfont);
+    password.setlimit(true, 20);
     while (window.isOpen())
     {
         sf::Event event;
@@ -73,10 +75,10 @@ void loginWindow(sf::RenderWindow& window, bool role)
             {
                 int x_coor = event.mouseButton.x;
                 int y_coor = event.mouseButton.y;
-                if (x_coor > 490 && x_coor < 1230)
+                if (x_coor > 60 && x_coor < 360)
                 {
                     //choose username input box
-                    if (y_coor > 545 && y_coor < 625) {
+                    if (y_coor > 500 && y_coor < 580) {
                         if (password.isselectedbox() == true)
                         {
                             password.setselected(false);
@@ -86,7 +88,7 @@ void loginWindow(sf::RenderWindow& window, bool role)
                         username.setselected(true);
                         acc.isselectedusername = true;
                     }
-                    else if (y_coor > 675 && y_coor < 755) {
+                    else if (y_coor > 640 && y_coor < 720) {
                         if (username.isselectedbox() == true) {
                             acc.isselectedusername = false;
                             username.setselected(false);
@@ -126,9 +128,9 @@ void loginWindow(sf::RenderWindow& window, bool role)
         }
         window.clear();
         window.draw(s_loginscreen);
-        username.setTextPosition(sf::Vector2f(500, 570));
+        username.setTextPosition(sf::Vector2f(60, 530));
         username.drawTextbox(window);
-        password.setTextPosition(sf::Vector2f(500, 700));
+        password.setTextPosition(sf::Vector2f(60, 670));
         password.drawTextbox(window);
         window.display();
     }
