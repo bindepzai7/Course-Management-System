f9d24567aff0bf42cfe99585fd3215e723a6e864
nguyen166 <trongnguyentran1606@gmail.com>
2024-03-18 15:51:04 +0700

    delete fucntion.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,23 +1,7 @@
-#include"displayfunction.h"
-#include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(1296, 838), "SFML works!");
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
 
-        window.clear();
-        chooseRole(window);
-        window.display();
-    }
 
+int main(){
     return 0;
 }
