c75c7433744388bda4ff673649def667c097dd35
nguyen166 <trongnguyentran1606@gmail.com>
2024-03-18 15:21:19 +0700

    delete main

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,19 +1,7 @@
-#include "LinkedList.h"
-#include "Student.h"
+
+
+
 
 int main() {
-    LinkedList<Student> studentList;
-    studentList.addNodeInAscending(Student("23125021", Name("Duc", "Tuan"), "290703", 0, Date(29,7,2005), "000090"));
-    studentList.addNodeInAscending(Student("23125017", Name("Duc", "Tuan"), "290708", 0, Date(29, 7, 2005), "000090"));
-    studentList.addNodeInAscending(Student("23125015", Name("Duc", "Tuan"), "290705", 0, Date(29, 7, 2005), "000090"));
-    studentList.addNodeInAscending(Student("23125015", Name("Duc", "Tuan"), "290705", 0, Date(29, 7, 2005), "000090"));
-    if (studentList.findXData(Student("23125017", "290703")))
-        std::cout << "OK";
-    else std::cout << "username or password is not true, please try again";
-    /*Node<Student>* curr = studentList.head;
-    while (curr) {
-        std::cout << curr->data.studentID << std::endl;
-        curr = curr->next;
-    }
-    */
+    return 0;
 }
