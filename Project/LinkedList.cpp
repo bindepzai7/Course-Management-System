1de07e6aceac9adab9ad25c7417d2f1b3c1db366
ntUyen2808 <161124872+ntUyen2808@users.noreply.github.com>
2024-03-20 09:50:47 +0700

    Implement basic Course function

---
+++ Project/LinkedList.cpp
@@ -0,0 +1,82 @@
+
+#include "LinkedList.h"
+
+template <class T>
+LinkedList<T>::LinkedList() {
+    head = nullptr;
+}
+
+template <class T>
+LinkedList<T>::~LinkedList() {
+    while (head) {
+        Node<T>* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+template <class T>
+void LinkedList<T>::addNodeAtFront(T data) {
+    Node<T>* newNode = new Node<T>;
+    newNode->data = data;
+    newNode->next = nullptr;
+
+    if (!head) {
+        head = newNode;
+    }
+    else {
+        newNode->next = head;
+        head = newNode;
+    }
+}
+
+template <class T>
+void LinkedList<T>::addNodeInAscending(T data) {
+    Node<T>* newNode = new Node<T>;
+    newNode->data = data;
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        head = newNode;
+    }
+    else {
+        if (data < head->data) {
+            newNode->next = head;
+            head = newNode;
+        }
+        else if (data == head->data) return;
+        else {
+            Node<T>* curr = head;
+            while (curr->next != nullptr && curr->next->data < data) {
+                curr = curr->next;
+            }
+            if (curr->next && curr->next->data == data) return;
+            newNode->next = curr->next;
+            curr->next = newNode;
+        }
+    }
+}
+
+template <class T>
+void LinkedList<T>::display() {
+    Node<T>* temp = head;
+    while (temp) {
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+}
+
+template <class T>
+bool LinkedList<T>::isXDatainlist(T data) {
+    if (head == nullptr) return false;
+    if (!(head->data != data)) return true;
+    Node<T>* cur = head;
+    while (cur)
+    {
+        if (!(cur->data != data)) return true;
+        cur = cur->next;
+    }
+    return false;
+}
+};
--- Project/LinkedList.h
+++ Project/LinkedList.h
@@ -14,20 +14,24 @@ template <class T>
 class LinkedList {
 public:
     Node<T>* head;
+    LinkedList();
 
-    LinkedList() {
-        head = nullptr;
+    ~LinkedList();
+
+    void addNodeAtFront(T data);
+
+    //Additional functional NTU's made (please delete unwanted ones)
+    /////////////////////////////////////////////////////////////
+
+    void push_head(T data) {
+        Node<T>* newNode = new Node<T>;
+        newNode->data = data;
+        newNode->next = head;
+        head = newNode;
     }
 
-    ~LinkedList() {
-        while (head) {
-            Node<T>* temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
-
-    void addNodeAtFront(T data) {
+    //the following function is called in AddCourse.cpp
+    void push_tail(T data) {
         Node<T>* newNode = new Node<T>;
         newNode->data = data;
         newNode->next = nullptr;
@@ -36,61 +40,61 @@ public:
             head = newNode;
         }
         else {
-            newNode->next = head;
-            head = newNode;
+            Node<T>* temp = head;
+            while (temp->next) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
         }
     }
 
-    void addNodeInAscending(T data) {
-        Node<T>* newNode = new Node<T>;
-        newNode->data = data;
-        newNode->next = nullptr;
-
-        if (head == nullptr) {
-            head = newNode;
-        }
-        else {
-            if (data < head->data) {
-                newNode->next = head;
-                head = newNode;
-            }
-            else if (data == head->data) return;
-            else{
-                Node<T>* curr = head;
-                while (curr->next != nullptr && curr->next->data < data) {
-                    curr = curr->next;
-                }
-                if (curr->next && curr->next->data == data) return;
-                newNode->next = curr->next;
-                curr->next = newNode;
-            }
-        }
-    }
-
-    void display() {
+    void pop_head(T& data) {
+        if (!head) return;
+        data = head->data;
         Node<T>* temp = head;
-        while (temp) {
-            std::cout << temp->data << " ";
+        head = head->next;
+        delete temp;
+    }
+
+    void pop_tail(T& data) {
+        if (!head) return;
+        Node<T>* temp = head;
+        while (temp->next and temp->next->next) {
             temp = temp->next;
         }
-        std::cout << std::endl;
+        if (!temp->next) {
+            data = temp->data;
+            head = nullptr;
+            delete temp;
+        }
+        else {
+            data = temp->next->data;
+            delete temp->next;
+            temp->next = nullptr;
+        }
     }
 
-    bool isXDatainlist(T data) {
-        if (head == nullptr) return false;
-        if (!(head->data != data)) return true;
-        Node<T>* cur = head;
-        while (cur)
-        {
-            if (!(cur->data != data)) return true;
+    void deleteAt(int index) {
+        Node<T>* cur = this->head;
+        for (int i = 1; i < index && cur->next != NULL; ++i) {
             cur = cur->next;
         }
-        return false;
+        if (cur->next != NULL) {
+            Node<T>* temp = cur->next;
+            cur->next = cur->next->next;
+            delete temp;
+        }
     }
+
+    ///////////////////////////////////////////////////////
+
+
+    void addNodeInAscending(T data);
+
+    void display();
+
+    bool isXDatainlist(T data);
 };
-
-
-
 
 
 
