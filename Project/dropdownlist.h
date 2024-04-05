559329590c4966fbf32affa460c6f5c5ea35cbe9
nguyen166 <trongnguyentran1606@gmail.com>
2024-04-06 00:00:39 +0700

    fix list button

--- Project/dropdownlist.h
+++ Project/dropdownlist.h
@@ -12,6 +12,11 @@ private:
 	LinkedList<Button> buttonlist;
 public:
 	dropdownlist() {}
+	dropdownlist(sf::Color color, sf::Vector2f size, bool isClicked, int numsofbutton) {
+		for (int i = 0; i < numsofbutton; i++) {
+			buttonlist.push_tail(Button(color, size, isClicked));
+		}
+	}
 	dropdownlist(sf::Color color, sf::Vector2f size, bool isClicked,
 		sf::Color textColor, LinkedList<std::string>& textlist, int txtsize, sf::Font& font)
 	{
@@ -21,15 +26,35 @@ public:
 			cur = cur->next;
 		}
 	}
-	void drawto(sf::RenderWindow& window, sf::Event event,float Posx,float Posy,float size, int i) {
+
+	//position of button without textbox
+	void drawButwithoutTextbox(sf::RenderWindow& window, sf::Event event, float Posx, float Posy, int i, float size,sf::Color color) {
+		Node<Button>* curbut = buttonlist.head;
+		while (curbut) {
+			curbut->data.setButposition(sf::Vector2f(Posx, Posy + size * i));
+			if (curbut->data.isonMousecursor(event)) {
+				curbut->data.changecolor(color);
+			}
+			else
+				curbut->data.changecolor(sf::Color::Transparent);
+			curbut->data.drawbutton(window);
+			i++;
+			curbut = curbut->next;
+		}
+	}
+	//position of button with textbox
+	void drawButwithTextbox(sf::RenderWindow& window, sf::Event event, float Posx, float Posy, int i, float size, sf::Color color,sf::Color txtcolor) {
 		Node<Button>* curbut = buttonlist.head;
 		while (curbut) {
 			curbut->data.setposition(sf::Vector2f(Posx, Posy + size * i));
 			if (curbut->data.isonMousecursor(event)) {
-				curbut->data.changecolor(sf::Color::White);
+				curbut->data.changecolor(color);
+				curbut->data.changeTextColor(txtcolor);
 			}
-			else
+			else {
 				curbut->data.changecolor(sf::Color::Transparent);
+				curbut->data.changeTextColor(sf::Color::Transparent);
+			}
 			curbut->data.drawbutton(window);
 			i++;
 			curbut = curbut->next;
