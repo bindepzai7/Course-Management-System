a0efb51a721d8b1e742600886103e990b7ef946f
Nguyen Thu Uyen <161124872+ntUyen2808@users.noreply.github.com>
2024-04-16 05:45:31 +0700

    Add staff change password screen

--- Project/staffview.cpp
+++ Project/staffview.cpp
@@ -123,6 +123,9 @@ void staffviewprofile(sf::RenderWindow& window, Staff& userstaff) {
                     if (x_coor > 45 && x_coor < 100 && y_coor>47 && y_coor < 100) {
                         staffhome(window, userstaff);
                     }
+                    if (x_coor > 1087 && x_coor < 1200 && y_coor> 370 && y_coor < 405) {
+                        staffChangePassword(window, userstaff);
+                    }
                 }
             }
             if (staffhomebuttonlist.isClickedKOrder(event, 1))
@@ -152,6 +155,87 @@ void staffviewprofile(sf::RenderWindow& window, Staff& userstaff) {
 
         window.clear();
         window.draw(s_staffViewProfiletexture);
+        staffhomebuttonlist.drawButwithTextbox(window, event, sf::Color(168, 158, 146), sf::Color(239, 233, 222));
+        logoutbut.drawbutton(window);
+        window.display();
+
+    }
+}
+
+void staffChangePassword(sf::RenderWindow& window, Staff& userstaff) {
+    sf::Texture staffChangePassword;
+    staffChangePassword.loadFromFile("Design UI/[Staff] Change Password.jpg");
+    staffChangePassword.setSmooth(true);
+    sf::Sprite s_staffChangePassword;
+    s_staffChangePassword.setTexture(staffChangePassword);
+    sf::Font Palatino;
+    Palatino.loadFromFile("Font/Palatino.ttf");
+
+    //list of button manage
+    LinkedList<std::string> textstaffhomebutton;
+    textstaffhomebutton.push_tail(" manage school year ");
+    textstaffhomebutton.push_tail("  manage semester   ");
+    textstaffhomebutton.push_tail("   manage course    ");
+    textstaffhomebutton.push_tail("    manage class    ");
+    textstaffhomebutton.push_tail("      about us      ");
+    dropdownlist staffhomebuttonlist(sf::Color(168, 158, 146), sf::Vector2f(280, 53), false, sf::Color(239, 233, 222), textstaffhomebutton, 30, Palatino);
+    staffhomebuttonlist.setpostionlistbutton(30, 143, 0, 65);
+
+
+
+    //log out button
+    Button logoutbut(sf::Color(192, 200, 184), sf::Vector2f(90, 30), false, sf::Color::Black, "Log out", 20, Palatino);
+    logoutbut.setposition(sf::Vector2f(227, 895));
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    int x_coor = event.mouseButton.x;
+                    int y_coor = event.mouseButton.y;
+                    if (x_coor > 45 && x_coor < 100 && y_coor>47 && y_coor < 100) {
+                        staffhome(window, userstaff);
+                    }
+                    if (x_coor > 40 && x_coor < 77 && y_coor>887 && y_coor < 932) {
+                        staffviewprofile(window, userstaff);
+                    }
+                }
+            }
+            if (staffhomebuttonlist.isClickedKOrder(event, 1))
+                staffmanageschoolyeardisplay(window, userstaff);
+            if (staffhomebuttonlist.isClickedKOrder(event, 2))
+                staffmanagesemesterdisplay(window, userstaff);
+            if (staffhomebuttonlist.isClickedKOrder(event, 3))
+                staffmanagecourse(window, userstaff);
+            if (staffhomebuttonlist.isClickedKOrder(event, 4))
+                staffmanageclass(window, userstaff);
+            if (staffhomebuttonlist.isClickedKOrder(event, 5))
+                staffaboutUs(window, userstaff);
+
+            //click logout button
+            if (logoutbut.isClick(event)) chooseRole(window);
+
+            //change color button when on cursor
+            if (logoutbut.isonMousecursor(event)) {
+                logoutbut.changecolor(sf::Color(192, 200, 184));
+                logoutbut.changeTextColor(sf::Color::Black);
+            }
+            else {
+                logoutbut.changecolor(sf::Color::Transparent);
+                logoutbut.changeTextColor(sf::Color::Transparent);
+            }
+        }
+
+        window.clear();
+        window.draw(s_staffChangePassword);
         staffhomebuttonlist.drawButwithTextbox(window, event, sf::Color(168, 158, 146), sf::Color(239, 233, 222));
         logoutbut.drawbutton(window);
         window.display();
--- Project/staffview.h
+++ Project/staffview.h
@@ -9,34 +9,27 @@
 #include<SFML/Graphics.hpp>
 #include<Windows.h>
 
-
-void staffaboutUs(sf::RenderWindow& window, Staff& userstaff);
-void staffviewprofile(sf::RenderWindow& window, Staff& userstaff);
-
 void staffhome(sf::RenderWindow& window, Staff& userstaff);
 
+void staffviewprofile(sf::RenderWindow& window, Staff& userstaff);
+
+void staffChangePassword(sf::RenderWindow& window, Staff& userstaff);
 
 void staffmanageschoolyeardisplay(sf::RenderWindow& window, Staff& userstaff);
-
 
 void staffmanageschoolyear2display(sf::RenderWindow& window, Staff& userstaff, int Korderofbut);
 
 void staffaddclasses(sf::RenderWindow& window, Staff& userstaff, std::string schoolyear);
 
-
 void staffaddfirstyearstudent(sf::RenderWindow& window, Staff& userstaff, std::string schoolyear, std::string addtoclass);
-
 
 std::string filenametoimport();
 
-
-
 void staffmanagesemesterdisplay(sf::RenderWindow& window, Staff& userstaff);
 
-
-
-void staffmanagecourse(sf::RenderWindow& window, Staff& userstaff);;
-
-
+void staffmanagecourse(sf::RenderWindow& window, Staff& userstaff);
 
 void staffmanageclass(sf::RenderWindow& window, Staff& userstaff);
+
+void staffaboutUs(sf::RenderWindow& window, Staff& userstaff);
+
