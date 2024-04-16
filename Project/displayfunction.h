0565c18675e017f1baa508c2b0bc93cba18a50c6
Nguyen Thu Uyen <161124872+ntUyen2808@users.noreply.github.com>
2024-04-16 19:23:42 +0700

    add 2 manage course screens

--- Project/displayfunction.h
+++ Project/displayfunction.h
@@ -48,7 +48,8 @@ void staffmanagesemesterdisplay(sf::RenderWindow& window,Staff &userstaff, std::
 //manage course
 void staffmanagecourse(sf::RenderWindow& window,Staff &userstaff, std::string schoolyear, std::string semester);
 void staffaddcourse(sf::RenderWindow& window, Staff& userstaff, std::string schoolyear, std::string semester);
-
+void staffviewstudentofcourse(sf::RenderWindow& window, Staff& userstaff, std::string schoolyear, std::string semester, std::string coursechosen);
+void staffaddstudentstocourse(sf::RenderWindow& window, Staff& userstaff, std::string schoolyear, std::string semester, std::string coursechosen);
 
 //manage class
 void staffaddclasses(sf::RenderWindow& window, Staff& userstaff, std::string schoolyear);
